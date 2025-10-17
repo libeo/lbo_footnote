3dcd40b371a1edbe61d5ce854c380e33f8560d3f
Maxime McKay-Turgeon <mmckayt@libeo.com>
2025-10-17 10:05:28 -0400

    [FIX] Fix french translations when adding it as an editor language

--- Resources/Public/JavaScript/footquote/lang/translations.js
+++ Resources/Public/JavaScript/footquote/lang/translations.js
@@ -3,15 +3,18 @@ export default function initTranslations() {
     window.CKEDITOR_TRANSLATIONS = window.CKEDITOR_TRANSLATIONS || {};
 
     // Make sure that the dictionary for french translations exist.
-    window.CKEDITOR_TRANSLATIONS[ 'fr' ] = window.CKEDITOR_TRANSLATIONS[ 'fr' ] || {};
-    window.CKEDITOR_TRANSLATIONS[ 'fr' ].dictionary =  window.CKEDITOR_TRANSLATIONS[ 'fr' ].dictionary || {};
+    window.CKEDITOR_TRANSLATIONS['fr_CA'] = window.CKEDITOR_TRANSLATIONS['fr_CA'] || {};
+    window.CKEDITOR_TRANSLATIONS['fr_CA'].dictionary = window.CKEDITOR_TRANSLATIONS['fr_CA'].dictionary || {};
 
     // Make sure that the dictionary for english translations exist.
-    window.CKEDITOR_TRANSLATIONS[ 'en' ] = window.CKEDITOR_TRANSLATIONS[ 'en' ] || {};
-    window.CKEDITOR_TRANSLATIONS[ 'en' ].dictionary =  window.CKEDITOR_TRANSLATIONS[ 'en' ].dictionary || {};
+    window.CKEDITOR_TRANSLATIONS['en'] = window.CKEDITOR_TRANSLATIONS['en'] || {};
+    window.CKEDITOR_TRANSLATIONS['en'].dictionary = window.CKEDITOR_TRANSLATIONS['en'].dictionary || {};
 
-    // Extend the dictionary for french translations with your translations:
-    Object.assign( window.CKEDITOR_TRANSLATIONS[ 'fr' ].dictionary, {
+    if (!window.CKEDITOR_TRANSLATIONS['fr_CA']) {
+        window.CKEDITOR_TRANSLATIONS['fr_CA'].getPluralForm = n => n == 1 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2
+    }
+
+    Object.assign(window.CKEDITOR_TRANSLATIONS['fr_CA'].dictionary, {
         'insertFootquote': 'Insérer une note de bas de page',
         'footquote': 'Note de bas de page',
         'footquoteDescription': 'Description',
@@ -21,15 +24,16 @@ export default function initTranslations() {
         'Save': 'Sauvegarder',
         'Cancel': 'Annuler',
         'Link': 'Lien'
-    } );
+    });
+
 
     // Extend the dictionary for english translations with your translations:
-    Object.assign( window.CKEDITOR_TRANSLATIONS[ 'en' ].dictionary, {
+    Object.assign(window.CKEDITOR_TRANSLATIONS['en'].dictionary, {
         'insertFootquote': 'Insert footquote',
         'footquote': 'Footquote',
         'footquoteDescription': 'Description',
         'explanationCannotBeEmpty': 'Description cannot be empty',
         'dialogTitle': 'Footquote',
         'editFootquote': 'Edit footquote',
-    } );
+    });
 }
